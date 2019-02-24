0cecb862d77d4a5ec783012ac2b8fef4f82f0f58
Akriti Bhusal <akriti.ab@gmail.com>
2019-02-24 16:41:30 +1100

    Add folder commands

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ var shell = require('shelljs')
 console.log('Copying base folder structure')
 
 shell.exec('mkdir project')
-shell.exec()
+shell.exec('cp -r ./myApp/* ./project')
+shell.exec('cd project')
+shell.exec('mkdir -p sass views/{layouts,templates,partials}')
+shell.exec('touch views/index.html')
+shell.exec('touch gulpfile.js package.json')
+
+
+
+shell.exec('npm install')
+
+console.log('Completed  without errors')
